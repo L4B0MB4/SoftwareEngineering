ab988b9c9c59651d3f09be82455ee89675068401
Lars <bommersbach1996@gmail.com>
2018-03-13 17:43:55 +0100

    Anderes Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
 import { Container, Grid, Menu, Input } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 class Layout extends Component {
-
-    constructor(props)
-    {
-        super(props);
-        this.state=
-        {
-            activeItem:"home"
-        }
-    }
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: "home"
+    };
+  }
 
   render() {
-      const activeItem = this.state.activeItem;
+    const activeItem = this.state.activeItem;
     return (
-      <Container style={{width:"100%"}}>
-      <Header activeItem={activeItem}/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+      <Container style={{ width: "100%" }}>
+        <Header activeItem={activeItem} />
+        <br />
+        <br />
+        <br />
+        <br />
         <Grid>
           <Grid.Row>
             <Grid.Column mobile={1} computer={2} />
@@ -36,33 +33,25 @@ class Layout extends Component {
   }
 }
 
-const Header = (props) => {
-   const {activeItem} = props;
+const Header = props => {
+  const { activeItem } = props;
   return (
     <div>
       <Menu inverted fixed="top" size="huge">
         <Menu.Item>
-          <img src='./logo.png' /> SemsaKrebsler
+          <img src="./logo.png" /> SemsaKrebsler
         </Menu.Item>
-        <Menu.Item
-          name="Hauptseite"
-          active={activeItem === "home"}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name="Weine"
-          active={activeItem === "messages"}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name="Lieferant"
-          active={activeItem === "friends"}
-          onClick={this.handleItemClick}
-        />
+        <Menu.Item>
+          <Link to="/">Anlegen</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/">Suche</Link>
+        </Menu.Item>
         <Menu.Menu position="right">
           <Menu.Item>
             <Input icon="search" placeholder="Search..." />
           </Menu.Item>
+          <Menu.Item>Abmelden</Menu.Item>
         </Menu.Menu>
       </Menu>
     </div>
